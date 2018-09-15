f59a9b3171737ba13a9d3e3505df83ca322f6e02
CountrySideEngineer <deep-sleeper@nify.com>
2018-09-15 21:59:09 +0900

    Depend on application running platform, the application window size will be changed. -To make application window size be able to change, add declaration of pre-processor, which is used just after when the application is started. -In windows platform, application is started in size of property value. In another, the application is started in full screen.

--- dev/src/QtSampleWatch/main.cpp
+++ dev/src/QtSampleWatch/main.cpp
@@ -5,7 +5,11 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QtWatchSampleWindow w;
+#ifdef PLATFORM_WIN32
     w.show();
+#else
+    w.showFullScreen();
+#endif
 
     return a.exec();
 }
